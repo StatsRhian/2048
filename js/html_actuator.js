4b491984e66dcf174fae9b784802f3c51bf15d02
trianglegirl <r.davies3@lancaster.ac.uk>
2014-03-21 06:04:17 +0000

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Fail! You're not STOR-i material.";
+  var message = won ? "You win!" : "Fail! Quit STOR-i.";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
